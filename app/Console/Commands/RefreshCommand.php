f72d67f87dc576007a296a237a399fef1333adc8
rayzenwp <marusichenko666@gmail.com>
2023-02-13 02:12:32 +0200

    todo

--- app/Console/Commands/RefreshCommand.php
+++ app/Console/Commands/RefreshCommand.php
@@ -17,11 +17,18 @@ class RefreshCommand extends Command
             return self::FAILURE;
         }
        
-        Storage::deleteDirectory('images/products');
+     //   Storage::deleteDirectory('images/products');
 
         $this->call('migrate:fresh',[
             '--seed' => true
         ]);
+
+    //     TODO: 
+    //     нужно из конфига права на папку доставать иначе будет ошибка
+    //     https://stackoverflow.com/questions/28962800/laravel-5-mkdir-filesystemmakedirectory-with-permissions-from-config
+    //     https://laravel.com/docs/9.x/filesystem#directories
+    //     https://stackoverflow.com/questions/64678596/laravel-store-files-with-permissions-774
+    //    Storage::createDirectory('images/products');
 
         return self::SUCCESS;
     }
